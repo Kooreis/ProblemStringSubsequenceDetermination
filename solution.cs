355929f0ee892a2ebb7904c708e8d0acacaf8108
Kooreis <kooreis.official@gmail.com>
2024-05-24 15:32:31 -0500

    Created the function isSubsequence with two parameters str1 and str2. Initialized a variable j to 0.

--- solution.cs
+++ solution.cs
@@ -1,15 +1,3 @@
-Here is a simple JavaScript console application that determines whether one string is a subsequence of another:
-
-```javascript
 function isSubsequence(str1, str2) {
     let j = 0;
-    for(let i = 0; i < str2.length; i++) {
-        if(str1[j] === str2[i]) j++;
-        if(j === str1.length) return true;
-    }
-    return false;
 }
-
-console.log(isSubsequence('abc', 'ahbgdc')); // true
-console.log(isSubsequence('axc', 'ahbgdc')); // false
-```
